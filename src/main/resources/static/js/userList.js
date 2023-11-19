2d3524e2d669ed3a574f40c79d5e57b8b39ea9e6
hi-lee-mon <shunmel.09@gmail.com>
2023-11-20 01:32:04 +0900

    [add]行選択処理の実装
    削除ボタン押下時のダイアログ表示処理の実装

---
+++ src/main/resources/static/js/userList.js
@@ -0,0 +1,54 @@
+// document.querySelectorAll('#userList tbody tr').forEach(function(row) {
+//   row.addEventListener('click', function() {
+//       // すべての行の選択状態を解除
+//       document.querySelectorAll('#userList tbody tr').forEach(function(otherRow) {
+//           otherRow.classList.remove('table-active');
+//       });
+
+//       // クリックされた行に選択状態のクラスを追加
+//       this.classList.add('table-active');
+
+//       // 更新ボタン、削除ボタンを活性化
+//       document.getElementById('updateBtn').removeAttribute('disabled');
+//       document.getElementById('deleteDummyBtn').removeAttribute('disabled');
+
+//       // ログインID一時保管
+//       editSelectedLoginId(this);
+//   });
+// });
+
+
+
+/**
+ * ユーザー一覧画面
+ */
+$(function() {
+	// テーブルの行をクリックしたときの処理
+	$('#userList tbody tr').on('click', function() {
+		// すべての行の選択状態を解除
+		$('#userList tbody tr').removeClass('table-active');
+		// クリックされた行に選択状態のクラスを追加
+		$(this).addClass('table-active');
+		// 更新ボタン、削除ボタンを活性化
+		$('#updateBtn').removeAttr('disabled');
+		$('#deleteDummyBtn').removeAttr('disabled');
+		
+		// ログインID一時保管
+		editSelectedLoginId($(this));
+	});
+});
+
+/**
+ * テーブルで選択された行のログインIDを画面のhidden要素に保管します。
+ * 
+ * @param row 選択された行情報
+ */
+function editSelectedLoginId(row) {
+	row.find('td').each(function() {
+		var columnId = $(this).attr('id');
+		if (columnId.startsWith('loginId_')) {
+			$('#selectedLoginId').val($(this).text());
+			return false;
+		}
+	});
+}
